8bf2291c5fab034857ed05882a3dec12d19ad617
YhtiPyhti <sergejsysoev565@gmail.com>
2022-04-12 15:02:04 +0700

    лор

--- Lab_4/Lab_4.cpp
+++ Lab_4/Lab_4.cpp
@@ -42,6 +42,6 @@ int main() {
     setlocale(LC_ALL, "Russian");
     //zad_1();
     //zad_2();
-    //zad_3();
-    //zad_4();
+    zad_3();
+   //zad_4();
 }
--- Lab_4/Zad_3.cpp
+++ Lab_4/Zad_3.cpp
@@ -6,44 +6,89 @@
 #include "Header.h"
 using namespace std;
 void zad_3() {
-    double Vn = 50, Vexp = 1, x = 0, x1 = 0, x2 = 0, MatOzOz = 50, MatOzReal = 0;
-    int k = 0, max = 0, max1 = 0, max2 = 0;
-    vector<int> arr, arr1, arr2;
+    double Vn = 50, Vexp = 1, Nt = 0.1 * 10, x = 0, r = 0;
+    int k = 0, max = 0, max1 = 0, max2 = 0, N = 0;
+    vector<int> arr, arr1, arr2, v;
+
     mt19937 gen(time(0));
     uniform_int_distribution<int> uid1(1, 100);
+
     for (size_t i = 0; i < 50; i++) {
         arr.push_back(uid1(gen));
-        x += (Vn - Vexp) * (Vn - Vexp) / Vexp;
-    }
-    for (size_t i = 0; i < 100; i++) {
-        arr1.push_back(uid1(gen));;
-        x1 += (Vn - Vexp) * (Vn - Vexp) / Vexp;
-    }
-    for (size_t i = 0; i < 1000; i++) {
-        arr2.push_back(uid1(gen));
-        x2 += (Vn - Vexp) * (Vn - Vexp) / Vexp;
     }
 
-    for (size_t i = 0; i < 49; i++) {
-        if (arr[i] == arr[i + 1]) {
-            k++;
-            if (k > max) max = k;
+    for (size_t j = 0; j < 10; j++){
+        N = 0;
+        for (size_t i = 0; i < 50; i++) {
+            if (arr[i] > j*10 && arr[i] < j*10 + 10) {
+                N++;
+            }
         }
-        else k = 0;
+        v.push_back(N);
     }
-    for (size_t i = 0; i < 99; i++) {
-        if (arr1[i] == arr1[i + 1]) {
-            k++;
-            if (k > max1) max1 = k;
+    
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (auto i : v) {
+        r = i / 50;
+        x += pow((r - Nt), 2) / Nt;
+    }
+    cout << "x^2 = " << x;
+    v.clear();
+
+    for (size_t i = 0; i < 100; i++) {
+        arr1.push_back(uid1(gen));
+    }
+
+
+    for (size_t j = 0; j < 10; j++) {
+        N = 0;
+        for (size_t i = 0; i < 100; i++) {
+            if (arr1[i] > j * 10 && arr1[i] < j * 10 + 10) {
+                N++;
+            }
         }
-        else k = 0;
+        v.push_back(N);
     }
-    for (size_t i = 0; i < 999; i++) {
-        if (arr2[i] == arr2[i + 1]) {
-            k++;
-            if (k > max2) max2 = k;
+
+    cout << endl;
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (auto i : v) {
+        r = i / 100;
+        x += pow((r - Nt), 2) / Nt;
+    }
+    cout << "x^2 = " << x;
+    v.clear();
+
+    for (size_t i = 0; i < 1000; i++) {
+        arr2.push_back(uid1(gen));
+    }
+
+
+    for (size_t j = 0; j < 10; j++) {
+        N = 0;
+        for (size_t i = 0; i < 1000; i++) {
+            if (arr2[i] > j * 10 && arr2[i] < j * 10 + 10) {
+                N++;
+            }
         }
-        else k = 0;
+        v.push_back(N);
     }
-    cout << x << " " << max << " " << x1 << " " << max1 << " " << x2 << " " << max2;
+
+    cout << endl;
+    for (auto i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (auto i : v) {
+        r = i / 1000;
+        x += pow((r - Nt), 2) / Nt;
+    }
+    cout << "x^2 = " << x;
+
 }
